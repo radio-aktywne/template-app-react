9ec6d3c94e519ac099d3724395166185ceefa4cc
Sebastian Pietras <me@spietras.dev>
2025-02-26 21:51:27 +0100

    Updated dependencies (#41)

--- src/src/store/types.ts
+++ src/src/store/types.ts
@@ -1,16 +1,16 @@
 import { StateCreator } from "zustand";
 import { PersistOptions } from "zustand/middleware";
 
-type StoreState = {
-  color: "green" | "red";
-};
-
-type StoreActions = {
-  flipColor: () => void;
-};
-
 export type Store = StoreActions & StoreState;
 
 export type StoreInitializer = StateCreator<Store, [["zustand/immer", never]]>;
 
 export type StorePersistOptions = PersistOptions<Store>;
+
+type StoreActions = {
+  flipColor: () => void;
+};
+
+type StoreState = {
+  color: "green" | "red";
+};
